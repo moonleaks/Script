d273d9f07eb5e7c499abb73bd040843e1a02ad96
moon <111334456+moonleaks@users.noreply.github.com>
2024-03-06 22:52:47 -0800

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Mori Scripts | :blossom: Pet Simulator 99! :blossom: ", "Midnight")
+local Window = Library.CreateLib("Crystal Scripts | :blossom: Pet Simulator 99! :blossom: ", "Midnight")
 
 -- MAIN
 local Main = Window:NewTab("Scripts")
